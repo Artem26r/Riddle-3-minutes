eb0465d537b024955653266c367bdc37af98adda
Artem <35739625+Artem26r@users.noreply.github.com>
2022-11-18 22:23:29 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+let riddle = {
+    question: 'Висит груша нельзя скушать',
+    correctAnswer: 'лампочка',
+    hints: ['Это съедобное', 'это фрукт. '],
+    tries: 3,
+    checkAnswer(str) {
+        if (this.tries < 1) {
+            return alert('Игра окончена');
+        }
+        if (str.toLowerCase().includes(this.correctAnswer)) {
+            alert('Правильный ответ');
+            this.tries = 0;
+        } else {
+            alert('Неправильный ответ');
+            this.tries--;
+
+            const hint = this.hints[this.tries === 2 ? 0 : 1];
+
+            if (this.tries) {
+                alert('Подсказка: ' + hint);
+            }
+        }
+    },
+}
+window.onload = function() {
+    document.getElementById('riddle').innerText = riddle.question;
+}
+
+function check() {
+    let input = document.getElementsByTagName('input')[0];
+
+    let guessedAnswer = input.value;
+
+    if (guessedAnswer) {
+        riddle.checkAnswer(guessedAnswer)
+    }
+}
+
+
